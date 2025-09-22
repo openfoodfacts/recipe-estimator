ab33bad443c790ce0f1503cc412720e8b7f6666a
John Gomersall <thegoms@gmail.com>
2025-09-22 18:39:33 +0100

    Simple harmonic model
    Signed-off-by: John Gomersall <thegoms@gmail.com>

---
+++ recipe_estimator/recipe_estimator_simple.py
@@ -0,0 +1,43 @@
+import time
+import numpy
+
+
+from .fitness import get_objective_function_args, objective
+
+POWER = -1.0
+def estimate_percentages(ingredients, total = 100):
+    # Each ingredient quantity = a * n ^ p
+    # where p is the POWER constant, n is the ingredient number and a is the percentage of the first ingredient
+    # We work out a by adding up all the results of the series with a = 1 and then factor a so that the total adds up to 100% (total)
+    num_ingredients = len(ingredients)
+    if num_ingredients < 1:
+        return
+    
+    raw_sum = sum([(n + 1) ** POWER for n in range(num_ingredients)])
+    a = total / raw_sum
+    for n, ingredient in enumerate(ingredients):
+        estimate = round(a * (n + 1) ** POWER, 2)
+        ingredient["percent_estimate"] = estimate
+        ingredient["quantity_estimate"] = estimate
+        if "ingredients" in ingredient:
+            estimate_percentages(ingredient["ingredients"], estimate)
+    return
+    
+    
+def estimate_recipe(product):
+    current = time.perf_counter()
+    [bounds, leaf_ingredients, args] = get_objective_function_args(product)
+    recipe_estimator = product['recipe_estimator']
+    
+    estimate_percentages(product["ingredients"])
+    recipe_estimator["status"] = 0
+    recipe_estimator["status_message"] = f"OK"
+
+    solution_x = numpy.array([ingredient['quantity_estimate'] for ingredient in leaf_ingredients])
+    objective(solution_x, *args)
+    recipe_estimator['penalties'] = args[0]
+    recipe_estimator["time"] = round(time.perf_counter() - current, 2)
+    message = f"Product: {product.get('code')}, time: {recipe_estimator['time']} s"
+    print(message)
+
+    return
---
+++ recipe_estimator/recipe_estimator_simple_test.py
@@ -0,0 +1,114 @@
+from recipe_estimator.nutrients import prepare_product
+from .recipe_estimator_simple import estimate_recipe
+
+def test_estimate_recipe_simple_recipe_with_one_unmatched_ingredient():
+    product = {
+        'code': 'test', 
+        'ingredients': [
+            {
+                'id':'one',
+                'nutrients': {
+                    'fiber': {'percent_nom': 15, 'percent_min': 0, 'percent_max': 100},
+                }
+            },
+            {
+                'id':'two',
+                'nutrients': {
+                    'fiber': {'percent_nom': 0, 'percent_min': 0, 'percent_max': 100},
+                }
+            }
+        ],
+        'nutriments': {
+            'fiber_100g': 10,
+        }}
+
+    estimate_recipe(product)
+
+    metrics = product.get('recipe_estimator')
+    assert metrics is not None
+
+    # Status is valid
+    #assert metrics['status'] == 0
+
+    assert round(product['ingredients'][0]['percent_estimate']) >= 50
+    assert round(product['ingredients'][1]['percent_estimate']) <= 50
+    assert round(product['ingredients'][0]['percent_estimate'] + product['ingredients'][1]['percent_estimate']) == 100
+
+def test_estimate_recipe_subingredients():
+    product = {
+        'code': 'test', 
+        'ingredients': [
+            {
+                'id':'en:tomato',
+                'nutrients': {
+                    'fiber': {'percent_nom': 5, 'percent_min': 5, 'percent_max': 5},
+                    'water': {'percent_nom': 90, 'percent_min': 0, 'percent_max': 100},
+                    'sugars': {'percent_nom': 0, 'percent_min': 0, 'percent_max': 0},
+                    'salt': {'percent_nom': 0, 'percent_min': 0, 'percent_max': 0},
+                }
+            },
+            {
+                'id':'en:sugar-and-salt',
+                'ingredients': [{
+                    'id':'en:sugar',
+                    'nutrients': {
+                        'sugars': {'percent_nom': 100, 'percent_min': 100, 'percent_max': 100},
+                        'fiber': {'percent_nom': 0, 'percent_min': 0, 'percent_max': 0},
+                        'salt': {'percent_nom': 0, 'percent_min': 0, 'percent_max': 0},
+                    }
+                },
+                {
+                    'id':'en:salt',
+                    'nutrients': {
+                        'salt': {'percent_nom': 100, 'percent_min': 100, 'percent_max': 100},
+                        'fiber': {'percent_nom': 0, 'percent_min': 0, 'percent_max': 0},
+                        'sugars': {'percent_nom': 0, 'percent_min': 0, 'percent_max': 0},
+                    }
+                }
+                ]
+            }
+        ],
+        'nutriments': {
+            'fiber_100g': 5,
+            'sugars_100g': 10,
+            'salt_100g': 5
+        }}
+
+    estimate_recipe(product)
+
+    # Print the resulting product structure
+    # print(json.dumps(product, indent=2))
+
+    metrics = product.get('recipe_estimator')
+    assert metrics is not None
+
+    tomatoes = product['ingredients'][0]['percent_estimate']
+    sugar = product['ingredients'][1]['ingredients'][0]['percent_estimate']
+    salt = product['ingredients'][1]['ingredients'][1]['percent_estimate']
+    assert tomatoes > sugar + salt
+    assert sugar > salt
+    assert abs(100 - (tomatoes + sugar + salt)) < 1
+
+
+def test_ingredients_dont_add_up():
+    product = {
+        'code' : 'test',
+        'ingredients': [
+            {'id':'en:sugar'},
+            {'id':'en:salt'},
+        ],
+        'nutriments': {
+            'sugars_100g': 80
+        }
+    }
+
+    prepare_product(product)
+    estimate_recipe(product)
+    metrics = product.get('recipe_estimator')
+    assert metrics is not None
+    
+    sugar = product['ingredients'][0]['quantity_estimate']
+    salt = product['ingredients'][1]['quantity_estimate']
+    assert sugar > salt
+    assert abs(100 - (sugar + salt)) < 1
+
