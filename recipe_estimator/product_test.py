9e1be51184eedfd5b3adbd82b20ebfd030efdb47
John Gomersall <thegoms@gmail.com>
2026-05-04 17:54:00 +0100

    Fix test CI detection
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- recipe_estimator/product_test.py
+++ recipe_estimator/product_test.py
@@ -2,7 +2,6 @@ import os
 from .product import get_product
 
 def test_product_comes_from_metrics():
-    product = get_product('20005726')
     is_ci = os.environ.get('CI', False)
     # Shouldn't see any non-whitelisted fields if came from metrics
-    assert is_ci or "_keywords" not in product
+    assert is_ci or "_keywords" not in get_product('20005726')
