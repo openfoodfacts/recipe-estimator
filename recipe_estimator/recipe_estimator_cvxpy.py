23f15b31486f2e1dba6936c93a0b549243d68faf
John Gomersall <thegoms@gmail.com>
2026-08-16 15:02:33 +0100

    Refactoring for clarity nad to address some issues
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- recipe_estimator/recipe_estimator_cvxpy.py
+++ recipe_estimator/recipe_estimator_cvxpy.py
@@ -23,12 +23,22 @@ def get_ingredient_range(ingredient_percentage):
 
 def add_ingredient_constraints(
     ingredients,
+    nutrients,
     constraints,
     leaf_ingredients,
     ingredient_quantities,
     water_proportions,
     ingredient_vars,
+    nutrients_from_ingredients = None,
 ):
+    is_top_level = nutrients_from_ingredients is None
+    if is_top_level:
+        nutrients_from_ingredients = {
+            'salt': cp.Constant(0),
+            'sugars': cp.Constant(0),
+            'fat': cp.Constant(0),
+        }
+
     previous_ingredient_mixing_bowl_weight = None
     total_mixing_bowl_weight = []
     for ingredient in ingredients:
@@ -46,11 +56,13 @@ def add_ingredient_constraints(
             ingredient_var["ingredients"] = []
             my_mixing_bowl_weight = add_ingredient_constraints(
                 ingredient["ingredients"],
+                nutrients,
                 constraints,
                 leaf_ingredients,
                 ingredient_quantities,
                 water_proportions,
                 ingredient_var["ingredients"],
+                nutrients_from_ingredients,
             )
             # Keep a note of how many child ingredients make up the total for this parent ingredient
             last_child_index = len(leaf_ingredients)
@@ -98,111 +110,109 @@ def add_ingredient_constraints(
                 ])
                 my_mixing_bowl_weight.append(-pre_mixing_bowl_water_loss)
 
+            ingredient_id = ingredient.get("id", "")
+
+            # Add constraint for flavour ingredients and food additives, except for first leaf ingredient
+            if my_index > 0:
+                if "flavour" in ingredient_id or (ingredient_id.startswith("en:e") and len(ingredient_id) > 4 and ingredient_id[4].isdigit()):
+                    constraints.append(my_quantity <= 2.0)
+
+            # If we are certain the ingredient contains a minimum amount of salt, sugar or fat
+            # we add it to the expressions so that we can add constraints to keep the total below the product's nutritional information
+            if ingredient_id == 'en:salt' or ingredient_id.endswith('-salt'):
+                nutrients_from_ingredients['salt'] += my_quantity
+            elif ingredient_id == 'en:sugar' or ingredient_id.endswith('-sugar'):
+                nutrients_from_ingredients['sugars'] += my_quantity
+            elif ingredient_id == 'en:honey' or ingredient_id.endswith('-honey'):
+                nutrients_from_ingredients['sugars'] += 0.6 * my_quantity
+            elif ingredient_id.endswith('-oil') or ingredient_id == 'en:cocoa-butter':
+                nutrients_from_ingredients['fat'] += my_quantity
+            elif ingredient_id.endswith('-fat'):
+                nutrients_from_ingredients['fat'] += 0.8 * my_quantity
+            elif ingredient_id == 'en:butter':
+                nutrients_from_ingredients['fat'] += 0.8 * my_quantity
+            elif ingredient_id == 'en:butterfat':
+                nutrients_from_ingredients['fat'] += 0.9 * my_quantity
+
         if previous_ingredient_mixing_bowl_weight and my_mixing_bowl_weight:
             constraints.append(sum(previous_ingredient_mixing_bowl_weight) >= sum(my_mixing_bowl_weight))
 
         total_mixing_bowl_weight.extend(my_mixing_bowl_weight)
         previous_ingredient_mixing_bowl_weight = my_mixing_bowl_weight
 
+    # If we are at the top level and we have nutritional information for the product
+    # we add constraints to keep the total salt, sugar and fat below the product's nutritional information
+    # We do this because processing (e.g. evaporation) should not reduce the total amount of salt, sugar or fat in the product
+    if is_top_level and constraints is not None and nutrients is not None:
+        salt = nutrients.get('salt')
+        if salt is not None:
+            constraints.append(nutrients_from_ingredients['salt'] <= ensure_float(salt))
+
+        sugars = nutrients.get('sugars')
+        if sugars is not None:
+            constraints.append(nutrients_from_ingredients['sugars'] <= ensure_float(sugars))
+
+        fat = nutrients.get('fat')
+        if fat is not None:
+            constraints.append(nutrients_from_ingredients['fat'] <= ensure_float(fat))
+
     return total_mixing_bowl_weight
 
 
 def estimate_percentages(
-    ingredient_quantities, nutrient_objectives, simple_objectives, ingredients, simple_estimates, total=100.0, index=0, percent_unknown=0, constraints=None, product_nutrients=None, is_first_product_ingredient=True, expressions=None, custom_estimates=None
+    ingredient_quantities,
+    nutrient_optimization_objectives,
+    simple_optimization_objectives,
+    ingredients,
+    initial_estimates,
+    total=100.0,
+    index=0,
+    percent_unknown=0,
 ):
-    # If called without custom estimates then we use a power-law series to estimate the percentages of each ingredient based on its position in the list of ingredients
+    # If called with a total then we use a power-law series to estimate the percentages of each ingredient based on its position in the list of ingredients
     # Each ingredient quantity = a * n ^ p
     # where p is the POWER constant, n is the ingredient number and a is the percentage of the first ingredient
     # We work out a by adding up all the results of the series with a = 1 and then factor a so that the total adds up to 100% (total)
+    # The initial_estimates array is then populated with these values. If no total is supplied we assume initial_estimates has already been populated.
     num_ingredients = len(ingredients)
     if num_ingredients < 1:
         return 0, 100
 
-    is_top_level = expressions is None
-    if is_top_level:
-        expressions = {
-            'salt': cp.Constant(0),
-            'sugars': cp.Constant(0),
-            'fat': cp.Constant(0),
-        }
+    if total is not None:
+        raw_sum = sum([(n + 1.0) ** POWER for n in range(num_ingredients)])
+        a = total / raw_sum
 
-    raw_sum = sum([(n + 1.0) ** POWER for n in range(num_ingredients)])
-    a = total / raw_sum
     for n, ingredient in enumerate(ingredients):
-        if custom_estimates is not None:
-            estimate = custom_estimates[index]
+        if total is None:
+            initial_estimate = initial_estimates[index]
         else:
-            estimate = round(a * (n + 1.0) ** POWER, 2)
+            initial_estimate = round(a * (n + 1.0) ** POWER, 2)
 
         if "ingredients" in ingredient and len(ingredient["ingredients"]) > 0:
             index, percent_unknown = estimate_percentages(
                 ingredient_quantities,
-                nutrient_objectives,
-                simple_objectives,
+                nutrient_optimization_objectives,
+                simple_optimization_objectives,
                 ingredient["ingredients"],
-                simple_estimates,
-                estimate,
+                initial_estimates,
+                initial_estimate if total is not None else None,
                 index,
                 percent_unknown,
-                constraints,
-                product_nutrients,
-                is_first_product_ingredient and n == 0,
-                expressions,
-                custom_estimates
             )
         else:
-            # If ingredient has no nutrient information then add an objective to keep close to the estimate
+            # If ingredient has no nutrient information then add an objective to keep close to the initial estimate
             if len(ingredient["nutrients"]) == 0:
-                percent_unknown += estimate
-                nutrient_objectives.append(
+                percent_unknown += initial_estimate
+                nutrient_optimization_objectives.append(
                     UNKNOWN_INGREDIENT_WEIGHTING
-                    * cp.square(ingredient_quantities[index] - estimate)
+                    * cp.square(ingredient_quantities[index] - initial_estimate)
                 )
-            # Simple objectives are used if we find that going by nutrients doesn't work
-            simple_objectives.append(cp.square(ingredient_quantities[index] - estimate))
-            simple_estimates.append(estimate)
+            # All ingredients aim to be as close as possible to the initial estimate when using simple optimization
+            simple_optimization_objectives.append(cp.square(ingredient_quantities[index] - initial_estimate))
+            if total is not None:
+                initial_estimates.append(initial_estimate)
 
-            ingredient_id = ingredient.get("id", "")
-            
-            # Add constraint for flavour ingredients and food additives, except for first product ingredient
-            if constraints is not None and not is_first_product_ingredient:
-                if "flavour" in ingredient_id or (ingredient_id.startswith("en:e") and len(ingredient_id) > 4 and ingredient_id[4].isdigit()):
-                    constraints.append(ingredient_quantities[index] <= 2.0)
-
-            # If we are certain the ingredient contains a minimum amount of salt, sugar or fat
-            # we add it to the expressions so that we can add constraints to keep the total below the product's nutritional information
-            if ingredient_id == 'en:salt' or ingredient_id.endswith('-salt'):
-                expressions['salt'] += ingredient_quantities[index]
-            elif ingredient_id == 'en:sugar' or ingredient_id.endswith('-sugar'):
-                expressions['sugars'] += ingredient_quantities[index]
-            elif ingredient_id == 'en:honey' or ingredient_id.endswith('-honey'):
-                expressions['sugars'] += 0.6 * ingredient_quantities[index]
-            elif ingredient_id.endswith('-oil') or ingredient_id == 'en:cocoa-butter':
-                expressions['fat'] += ingredient_quantities[index]
-            elif ingredient_id.endswith('-fat'):
-                expressions['fat'] += 0.8 * ingredient_quantities[index]
-            elif ingredient_id == 'en:butter':
-                expressions['fat'] += 0.8 * ingredient_quantities[index]
-            elif ingredient_id == 'en:butterfat':
-                expressions['fat'] += 0.9 * ingredient_quantities[index]
-            
             index += 1
-
-    # If we are at the top level and we have nutritional information for the product
-    # we add constraints to keep the total salt, sugar and fat below the product's nutritional information
-    # We do this because processing (e.g. evaporation) should not reduce the total amount of salt, sugar or fat in the product
-    if is_top_level and constraints is not None and product_nutrients is not None:
-        salt = product_nutrients.get('salt_100g')
-        if salt is not None:
-            constraints.append(expressions['salt'] <= ensure_float(salt))
-
-        sugars = product_nutrients.get('sugars_100g')
-        if sugars is not None:
-            constraints.append(expressions['sugars'] <= ensure_float(sugars))
-
-        fat = product_nutrients.get('fat_100g')
-        if fat is not None:
-            constraints.append(expressions['fat'] <= ensure_float(fat))
 
     return index, percent_unknown
 
@@ -246,8 +256,6 @@ def estimate_recipe(product, use_simple_estimates=False):
     recipe_estimator = product["recipe_estimator"]
     nutrients = recipe_estimator["nutrients"]
 
-    ingredients_nutrients = []
-    product_nutrients = []
     leaf_ingredients = []
     water_proportions = []
 
@@ -257,6 +265,7 @@ def estimate_recipe(product, use_simple_estimates=False):
     ingredient_vars = []
     add_ingredient_constraints(
         ingredients,
+        nutrients,
         constraints,
         leaf_ingredients,
         ingredient_quantities,
@@ -270,40 +279,12 @@ def estimate_recipe(product, use_simple_estimates=False):
         <= 100
     )
 
-    for nutrient_key in nutrients:
-        nutrient = nutrients[nutrient_key]
-
-        weighting = nutrient.get("weighting", 0)
-        # Skip nutrients that don't have a weighting
-        if weighting == 0:
-            continue
-
-        product_total = nutrient["product_total"]
-        product_nutrients.append(product_total)
-        nutrient_weightings.append(weighting)
-        ingredient_nutrients = []
-
-        for i, ingredient in enumerate(leaf_ingredients):
-            ingredient_nutrient = ingredient["nutrients"].get(nutrient_key, {})
-            ingredient_nutrient_percent = ingredient_nutrient.get("percent_nom", 0)
-            ingredient_nutrients.append(ingredient_nutrient_percent * 0.01)
-
-        ingredients_nutrients.append(ingredient_nutrients)
-        
-        # Tried adding a constraint that the minimum nutrient value for all ingredients can't exceed what is on the packaging
-        # but it didn't improve the results
-    
-    # Need an np.array in the matrix multiplication below
-    ingredients_nutrients = np.array(ingredients_nutrients)
-
     # Pass 1A: Run estimate_percentages to get initial estimates for the ingredient quantities and objectives
     # Initial estimates are based on a power-law series.
-    nutrient_objectives1 = []
     simple_objectives1 = []
-    simple_estimates1 = []
-    _, _ = estimate_percentages(
-        ingredient_quantities, nutrient_objectives1, simple_objectives1, ingredients, simple_estimates1,
-        constraints=constraints, product_nutrients=product.get('nutriments', {})
+    initial_estimates = []
+    estimate_percentages(
+        ingredient_quantities, [], simple_objectives1, ingredients, initial_estimates
     )
 
     # Pass 1B: Run the solver with simple objectives
@@ -315,55 +296,73 @@ def estimate_recipe(product, use_simple_estimates=False):
     prob1 = cp.Problem(cp.Minimize(sum(objectives1)), constraints)
     prob1.solve()
 
-    pass1_solution = ingredient_quantities.value if prob1.status == cp.OPTIMAL else simple_estimates1
+    pass1_solution = ingredient_quantities.value if prob1.status == cp.OPTIMAL else initial_estimates
 
     # Pass 2A: Run estimate_percentages again to get updated objectives + new percent unknown based on the solution from pass 1B (if we have one)
 
-    nutrient_objectives2 = []
-    simple_objectives2 = []
-    simple_estimates2 = []
-
-    custom_estimates = pass1_solution if pass1_solution is not None else None
-
-    _, percent_unknown2 = estimate_percentages(
-        ingredient_quantities, nutrient_objectives2, simple_objectives2, ingredients, simple_estimates2,
-        custom_estimates=custom_estimates,
-        constraints=constraints, product_nutrients=product.get('nutriments', {})
+    nutrient_objectives = []
+    _, percent_unknown = estimate_percentages(
+        ingredient_quantities, nutrient_objectives, [], ingredients, pass1_solution, total=None
     )
 
     # Pass 2B (optional): If we have nutrient information for the product and the ingredients,
     # and if the percent unknown is low, then we try to solve with nutrient objectives
 
-    try_nutrients = False if use_simple_estimates else (percent_unknown2 < 10 and len(leaf_ingredients[0]["nutrients"]))
+    try_nutrients = False if use_simple_estimates else (percent_unknown < 10 and len(leaf_ingredients[0]["nutrients"]))
 
     final_solution = pass1_solution
     final_prob = prob1
     nutrient_variance_value = None
+    
+    product_nutrients = []
+    if try_nutrients:
+        # Gather ingredient nutrient quantities to an objective to minimize nutrient variance
+        ingredients_nutrients = []
+        for nutrient_key in nutrients:
+            nutrient = nutrients[nutrient_key]
 
-    if try_nutrients and product_nutrients:
-        residual = ingredients_nutrients @ ingredient_quantities - product_nutrients
-        nutrient_variance = cp.sum(nutrient_weightings @ cp.square(residual))
-        nutrient_objectives2.append(nutrient_variance)
+            weighting = nutrient.get("weighting", 0)
+            # Skip nutrients that don't have a weighting
+            if weighting == 0:
+                continue
 
-        objectives2 = list(nutrient_objectives2)
-        objectives2.append(evaporation_cost)
+            product_total = nutrient["product_total"]
+            product_nutrients.append(product_total)
+            nutrient_weightings.append(weighting)
+            ingredient_nutrients = []
 
-        prob2 = cp.Problem(cp.Minimize(sum(objectives2)), constraints)
-        prob2.solve()
+            for i, ingredient in enumerate(leaf_ingredients):
+                ingredient_nutrient = ingredient["nutrients"].get(nutrient_key, {})
+                ingredient_nutrient_percent = ingredient_nutrient.get("percent_nom", 0)
+                ingredient_nutrients.append(ingredient_nutrient_percent * 0.01)
 
-        if prob2.status == cp.OPTIMAL:
-            nutrient_variance_value = nutrient_variance.value.item()
-            recipe_estimator["nutrient_variance"] = nutrient_variance_value
+            ingredients_nutrients.append(ingredient_nutrients)
+            
+            # Tried adding a constraint that the minimum nutrient value for all ingredients can't exceed what is on the packaging
+            # but it didn't improve the results
+        
+        # Only do nutrient optimization if we have at least one product nutrient with a weighting
+        if product_nutrients:
+            # Need an np.array in the matrix multiplication below
+            ingredients_nutrients = np.array(ingredients_nutrients)
 
-            if nutrient_variance_value <= 2500:
-                final_solution = ingredient_quantities.value
-                final_prob = prob2
-            else:
-                final_solution = pass1_solution
-                final_prob = prob1
-        else:
-            final_solution = pass1_solution
-            final_prob = prob1
+            residual = ingredients_nutrients @ ingredient_quantities - product_nutrients
+            nutrient_variance = cp.sum(nutrient_weightings @ cp.square(residual))
+            nutrient_objectives.append(nutrient_variance)
+
+            objectives2 = list(nutrient_objectives)
+            objectives2.append(evaporation_cost)
+
+            prob2 = cp.Problem(cp.Minimize(sum(objectives2)), constraints)
+            prob2.solve()
+
+            if prob2.status == cp.OPTIMAL:
+                nutrient_variance_value = nutrient_variance.value.item()
+                recipe_estimator["nutrient_variance"] = nutrient_variance_value
+
+                if nutrient_variance_value <= 2500:
+                    final_solution = ingredient_quantities.value
+                    final_prob = prob2
 
     if try_nutrients and product_nutrients and final_prob is prob1 and prob1.status == cp.OPTIMAL:
         product_nutrients_arr = np.array(product_nutrients)
